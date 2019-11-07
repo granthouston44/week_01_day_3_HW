b51ba9e51c41fb4b1aded1ea0893268fc6127b0c
Grant Houston <grant.houston44@hotmail.com>
2019-11-07 09:45:18 +0000

    updated after class recap

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -2,15 +2,18 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 stops.push("Edinburgh Waverley")
+stops << "Edinburgh Waverley"
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
 stops.insert(0, "Glasgow Queen St")
+stops.unshift("Glasgow Queen St")
 
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 stops.insert(4, "Polmont")
 
 # 4. Work out the index position of `"Linlithgow"`
 stops.index("Linlithgow") #index 5
+stops.find_index("Linlithgow")
 
 # 5. Remove `"Livingston"` from the array using its name
 stops.delete("Livingston")
@@ -19,6 +22,8 @@ stops.delete("Livingston")
 stops.delete_at(2)
 # 7. How many stops there are in the array?
  stops.length
+ stops.count
+ stops.size
 # 8. How many ways can we return `"Falkirk High"` from the array?
 stops[2]
 stops[-5]
@@ -30,7 +35,9 @@ stops.fetch(2)
 stops.fetch(-5)
 
 # 9. Reverse the positions of the stops in the array
-stops.reverse
+stops.reverse #this will only return as long as that code is being Rutherglen
+#example p stops.reverse - however if i was to p stops at end of code, it wouldn't return in reverse.
+#could store in a variable, or use stops.reverse!
 
 # 10. Print out all the stops using a for loop
 for each_stop in stops
